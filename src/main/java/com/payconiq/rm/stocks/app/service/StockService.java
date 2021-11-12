7af497ac0c982c48bc6a8af8ec9a6764c91bf08a
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2021-11-12 14:32:57 +0530

    [imple] add stock service with data/name validation + tests

---
+++ src/main/java/com/payconiq/rm/stocks/app/service/StockService.java
@@ -0,0 +1,40 @@
+package com.payconiq.rm.stocks.app.service;
+
+import com.payconiq.rm.stocks.app.model.Stock;
+import com.payconiq.rm.stocks.app.repository.StockRepository;
+import com.payconiq.rm.stocks.app.util.StockAppUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StockService {
+    @Autowired
+    private StockRepository stockRepository;
+
+    public Stock addNewStock(Stock stock) {
+        // check if the stack object has all required attributes
+        if(StockAppUtil.validateStock(stock)) {
+            // validate if the name is not there
+            Optional<Stock> stockByName = stockRepository.findByName(stock.getName());
+            if(stockByName.isPresent()) {
+                //TODO:: create custom exception
+                throw new RuntimeException("Stock with Name("+stock.getName()+") already found!");
+            }
+            // set timestamp values
+            long now = Instant.now().getEpochSecond();
+            stock.setLastUpdated(now);
+            stock.setTimeCreated(now);
+            // persist the stock object
+            return stockRepository.save(stock);
+        } else {
+            //TODO:: create custom exception
+            throw new RuntimeException("Invalid request data passed!");
+        }
+    }
+
+}
---
+++ src/test/java/com/payconiq/rm/stocks/app/service/StockServiceTest.java
@@ -0,0 +1,84 @@
+package com.payconiq.rm.stocks.app.service;
+
+import com.payconiq.rm.stocks.app.model.Stock;
+import com.payconiq.rm.stocks.app.repository.StockRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class StockServiceTest {
+
+    @Mock
+    private StockRepository mockRepository;
+
+    @InjectMocks
+    private StockService service;
+
+
+    @Test
+    public void testAddStockWithProperData() {
+        double currentPrice = 23.45;
+        String sname = "Stock1";
+        Stock s1 = new Stock();
+        s1.setCurrentPrice(currentPrice);
+        s1.setName(sname);
+
+        Mockito.when(mockRepository.findByName(sname)).thenReturn(Optional.empty());
+        Mockito.when(mockRepository.save(s1)).thenReturn(s1);
+
+        Stock actual = service.addNewStock(s1);
+        Assert.assertEquals(s1, actual);
+        Mockito.verify(mockRepository).findByName(sname);
+        Mockito.verify(mockRepository).save(s1);
+    }
+
+
+    @Test
+    public void testAddStockWithInvalidDataGetException() {
+         String sname = "Stock1";
+        Stock s1 = new Stock();
+        s1.setName(sname);
+
+        Mockito.when(mockRepository.findByName(sname)).thenReturn(Optional.empty());
+
+        try {
+           service.addNewStock(s1);
+           Assert.fail("Should not come here!");
+        } catch (Exception e) {
+            // Assert.assertTrue(e instanceof RuntimeException);
+            Assert.assertEquals("Invalid request data passed!", e.getMessage());
+            Mockito.verify(mockRepository, Mockito.never()).findByName(sname);
+            Mockito.verify(mockRepository, Mockito.never()).save(s1);
+        }
+    }
+
+    @Test
+    public void testAddStockWithValidDataButExistingStockGetException() {
+        double currentPrice = 23.45;
+        String sname = "Stock1";
+        Stock s1 = new Stock();
+        s1.setCurrentPrice(currentPrice);
+        s1.setName(sname);
+
+        Mockito.when(mockRepository.findByName(sname)).thenReturn(Optional.of(s1));
+
+        try {
+            service.addNewStock(s1);
+            Assert.fail("Should not come here!");
+        } catch (Exception e) {
+            // Assert.assertTrue(e instanceof RuntimeException);
+            Assert.assertEquals("Stock with Name(Stock1) already found!", e.getMessage());
+            Mockito.verify(mockRepository).findByName(sname);
+            Mockito.verify(mockRepository, Mockito.never()).save(s1);
+        }
+    }
+
+}
